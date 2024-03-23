2165611c301b9f6aa924cf19fa535821a5667597
Desi9nerd <swpark514@gmail.com>
2024-03-23 21:12:19 +0900

    TDGA_Projectile - 파이어볼 스폰

--- Source/TDRPG/Private/GAS/GameplayAbility/TDGA_Projectile.cpp
+++ Source/TDRPG/Private/GAS/GameplayAbility/TDGA_Projectile.cpp
@@ -1,14 +1,25 @@
-#include "GAS/GameplayAbility/TDGAProjectile.h"
+#include "GAS/GameplayAbility/TDGA_Projectile.h"
 #include "Actor/TDProjectile.h"
 #include "Interface/ICombat.h"
 
-void UTDGAProjectile::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+void UTDGA_Projectile::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
+	//SpawnProjectile();
+}
+
+//void UTDGA_Projectile::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+//	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+//{
+//	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+//}
+
+void UTDGA_Projectile::SpawnProjectile()
+{
 	// Projectile Actor를 서버에만 스폰시킨다.
 	// 서버가 스폰시킨 Projectile Actor는 replicated actor가 되어 클라이언트는 replicated된 버젼을 보게 된다.
-	if (false == HasAuthority(&ActivationInfo)) return; // 서버가 아닌 경우 예외처리
+	if (false == GetAvatarActorFromActorInfo()->HasAuthority()) return; // 서버가 아닌 경우 예외처리
 
 	IICombat* CombatInterface = Cast<IICombat>(GetAvatarActorFromActorInfo());
 	if (CombatInterface)
@@ -26,5 +37,4 @@ void UTDGAProjectile::ActivateAbility(const FGameplayAbilitySpecHandle Handle, c
 
 		Projectile->FinishSpawning(SpawnTransform);
 	}
-
 }
--- Source/TDRPG/Public/GAS/GameplayAbility/TDGA_Projectile.h
+++ Source/TDRPG/Public/GAS/GameplayAbility/TDGA_Projectile.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "CoreMinimal.h"
 #include "GAS/GameplayAbility/TDGA.h"
-#include "TDGAProjectile.generated.h"
+#include "TDGA_Projectile.generated.h"
 
 class ATDProjectile;
 
@@ -10,13 +10,20 @@ class ATDProjectile;
  */
 
 UCLASS()
-class TDRPG_API UTDGAProjectile : public UTDGA
+class TDRPG_API UTDGA_Projectile : public UTDGA
 {
 	GENERATED_BODY()
 
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	//virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Projectile")
+	void SpawnProjectile();
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<ATDProjectile> ProjectileClass; // Projectile Actor
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FGameplayTag EventTag;
 };
