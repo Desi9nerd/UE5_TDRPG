1c23498f3a3c7b7cb48aed723aa0c69a328b514d
Desi9nerd <swpark514@gmail.com>
2024-03-28 12:26:26 +0900

    TDAbilityTypes - FGameplayEffectContext 상속받아 생성

---
+++ Source/TDRPG/Public/GAS/GameplayEffectContext/TDAbilityTypes.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "TDAbilityTypes.generated.h"
+
+/** 
+ *  GameplayEffectContext를 상속받아 NetSerialize를 재정의
+ */
+
+USTRUCT(BlueprintType)
+struct FTDGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+
+};
