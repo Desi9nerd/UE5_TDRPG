1c2e75f8b6774cfa29655bcdf7a480f5f4089f27
Desi9nerd <swpark514@gmail.com>
2024-03-31 14:48:25 +0900

    BTTask_Attack 생성

---
+++ Source/TDRPG/Public/AI/BTTask/BTTask_Attack.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "BTTask_Attack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDRPG_API UBTTask_Attack : public UBTTask_BlueprintBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
