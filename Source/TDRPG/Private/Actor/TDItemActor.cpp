be944215864aea526f82c6fa81aa01b04a44a113
Desi9nerd <swpark514@gmail.com>
2024-04-29 15:41:32 +0900

    ItemActor 생성

---
+++ Source/TDRPG/Private/Actor/TDItemActor.cpp
@@ -0,0 +1,48 @@
+﻿#include "Actor/TDItemActor.h"
+#include "Components/SphereComponent.h"
+#include "Net/UnrealNetwork.h"
+
+ATDItemActor::ATDItemActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bReplicates = true;
+
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetupAttachment(GetRootComponent());
+	SphereCollision = CreateDefaultSubobject<USphereComponent>("SphereCollision");
+	SphereCollision->SetupAttachment(Mesh);
+}
+
+void ATDItemActor::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(ATDItemActor, bDestroyItem, COND_None, REPNOTIFY_Always);
+}
+
+void ATDItemActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Mesh->SetSimulatePhysics(true);
+}
+
+void ATDItemActor::SetItemQuantity(int32 InItemQuantity)
+{
+	ItemQuantity = InItemQuantity;
+}
+
+void ATDItemActor::OnDestroyRootItem(bool InbDestroyItem)
+{
+	bDestroyItem = InbDestroyItem;
+}
+
+void ATDItemActor::OnRep_DestroyItem()
+{
+	if (bDestroyItem)
+	{
+		Destroy(); // 액터 소멸.
+	}
+}
---
+++ Source/TDRPG/Public/Actor/TDItemActor.h
@@ -0,0 +1,81 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TDItemActor.generated.h"
+
+class USphereComponent;
+class UTexture2D;
+class ATDItemActor;
+
+/** 포션, 크리스탈 등의 액터
+ *
+ */
+
+UENUM(BlueprintType) // InfiniteGameplayEffect 해제 종류
+enum class EItemCategory
+{
+	Inventory,
+	None
+};
+
+USTRUCT()
+struct FItem
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly)
+	FText Name;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UTexture2D> Thumbnail;
+
+	UPROPERTY(EditDefaultsOnly)
+	FText Description;
+
+	UPROPERTY(EditDefaultsOnly)
+	EItemCategory ItemCategory;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<ATDItemActor> ItemClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	bool bConsumable;
+
+	UPROPERTY(EditDefaultsOnly)
+	bool bStackable;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 MaxStackSize;
+};
+
+UCLASS()
+class TDRPG_API ATDItemActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ATDItemActor();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereCollision;
+
+	UPROPERTY(EditDefaultsOnly)
+	FString ItemName;
+	UPROPERTY(EditDefaultsOnly)
+	int32 ItemQuantity;
+	UPROPERTY(ReplicatedUsing = OnRep_DestroyItem)
+	bool bDestroyItem;
+
+private:
+	void SetItemQuantity(int32 InItemQuantity);
+	void OnDestroyRootItem(bool InbDestroyItem);
+
+	UFUNCTION()
+	void OnRep_DestroyItem();
+};
