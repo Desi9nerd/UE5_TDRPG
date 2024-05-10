4051efa721f366eca7001c325d84692f6a0bae14
Desi9nerd <swpark514@gmail.com>
2024-05-10 23:48:09 +0900

    TDGA_DamageCombo

---
+++ Source/TDRPG/Private/GAS/GameplayAbility/TDGA_DamageCombo.cpp
@@ -0,0 +1,99 @@
+﻿#include "GAS/GameplayAbility/TDGA_DamageCombo.h"
+
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Character/TDBaseCharacter.h"
+
+UTDGA_DamageCombo::UTDGA_DamageCombo()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UTDGA_DamageCombo::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	//ATDBaseCharacter* TDBaseCharacter = CastChecked<ATDBaseCharacter>(ActorInfo->AvatarActor.Get());
+	//TDBaseCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+
+	UAbilityTask_PlayMontageAndWait* PlayAttackTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("PlayAttack"), ComboActionMontage, 1.0f, GetNextSection());
+	PlayAttackTask->OnCompleted.AddDynamic(this, &ThisClass::OnCompleteCallback);
+	PlayAttackTask->OnInterrupted.AddDynamic(this, &ThisClass::OnInterruptedCallback);
+	PlayAttackTask->ReadyForActivation();
+
+	StartComboTimer();
+}
+
+void UTDGA_DamageCombo::InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo)
+{
+	if (false == ComboTimerHandle.IsValid()) // 마지막 공격인 경우
+	{
+		HasNextComboInput = false; // 콤보 공격이 이어지지 않도록 false 설정
+	}
+	else // 마지막 공격이 아닌 경우
+	{
+		HasNextComboInput = true; // 다음 몽타주 색션이 재생되도록 true 설정
+	}
+}
+
+//void UTDGA_DamageCombo::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility)
+//{
+//	Super::CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
+//}
+
+void UTDGA_DamageCombo::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+
+	//ATDBaseCharacter* TDBaseCharacter = CastChecked<ATDBaseCharacter>(ActorInfo->AvatarActor.Get());
+	//TDBaseCharacter->GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Walking);
+
+	//** 콤보 초기화	
+	CurrentCombo = 0;
+	HasNextComboInput = false;
+}
+
+void UTDGA_DamageCombo::OnCompleteCallback()
+{
+	bool bReplicatedEndAbility = true;
+	bool bWasCancelled = false;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
+
+void UTDGA_DamageCombo::OnInterruptedCallback()
+{
+	bool bReplicatedEndAbility = true;
+	bool bWasCancelled = true;
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
+
+FName UTDGA_DamageCombo::GetNextSection()
+{
+	CurrentCombo = FMath::Clamp(CurrentCombo + 1, 1, MaxComboCount);
+	FName NextSection = *FString::Printf(TEXT("%s%d"), *MontageSectionNamePrefix, CurrentCombo);
+
+	return NextSection;
+}
+
+void UTDGA_DamageCombo::StartComboTimer() // 콤보 타이머
+{
+	int32 ComboIndex = CurrentCombo - 1;
+	ensure(EffectiveFrameCount.IsValidIndex(ComboIndex));
+
+	const float ComboEffectiveTime = EffectiveFrameCount[ComboIndex] / FrameRate; // 몇 초 이후에 점검할지 변수
+	if (ComboEffectiveTime > 0.0f)
+	{
+		GetWorld()->GetTimerManager().SetTimer(ComboTimerHandle, this, &ThisClass::CheckComboInput, ComboEffectiveTime, false);
+	}
+}
+
+void UTDGA_DamageCombo::CheckComboInput()
+{
+	ComboTimerHandle.Invalidate(); // 타이머 핸들 무력화
+
+	if (HasNextComboInput) // 다음 공격 입력이 있다면
+	{
+		MontageJumpToSection(GetNextSection()); // 다음 몽타주 색션으로 넘어감
+		StartComboTimer(); // 콤보 타이머 새로 시작
+		HasNextComboInput = false;
+	}
+}
---
+++ Source/TDRPG/Public/GAS/GameplayAbility/TDGA_DamageCombo.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "GAS/GameplayAbility/TDGA_Damage.h"
+#include "TDGA_DamageCombo.generated.h"
+
+class UAnimMontage;
+
+/**
+ * 
+ */
+UCLASS()
+class TDRPG_API UTDGA_DamageCombo : public UTDGA_Damage
+{
+	GENERATED_BODY()
+
+public:
+	UTDGA_DamageCombo();
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	//virtual void CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+	UFUNCTION()
+	void OnCompleteCallback();
+
+	UFUNCTION()
+	void OnInterruptedCallback();
+
+	FName GetNextSection();
+	void StartComboTimer();
+	void CheckComboInput();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Combo", Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UAnimMontage> ComboActionMontage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combo", Meta = (AllowPrivateAccess = "true"))
+	FString MontageSectionNamePrefix;
+	UPROPERTY(EditDefaultsOnly, Category = "Combo", Meta = (AllowPrivateAccess = "true"))
+	uint8 MaxComboCount;
+	UPROPERTY(EditDefaultsOnly, Category = "Combo", Meta = (AllowPrivateAccess = "true"))
+	float FrameRate;
+	UPROPERTY(EditDefaultsOnly, Category = "Combo", Meta = (AllowPrivateAccess = "true"))
+	TArray<float> EffectiveFrameCount;
+
+	uint8 CurrentCombo = 0;
+	FTimerHandle ComboTimerHandle;
+	bool HasNextComboInput = false;
+
+};
