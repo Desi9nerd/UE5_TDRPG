d870d2339862d6abd28a86089f7e48306aec5693
Desi9nerd <swpark514@gmail.com>
2024-06-21 21:02:13 +0900

    업데이트

--- Source/TDRPG/Public/Interface/IPlayer.h
+++ Source/TDRPG/Public/Interface/IPlayer.h
@@ -19,47 +19,47 @@ class TDRPG_API IIPlayer
 public:
 	UFUNCTION(BlueprintNativeEvent)
 	void AddToExp(int32 InExp);
-	virtual void AddToExpCPP(int32 InExp);
+	virtual void AddToExpCPP(int32 InExp) = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	void AddToPlayerLevelBP(int32 InPlayerLevel);
-	virtual void AddToPlayerLevel(int32 InPlayerLevel);
+	virtual void AddToPlayerLevel(int32 InPlayerLevel) = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	void AddToAttributePointsBP(int32 InAttributePoints);
-	virtual void AddToAttributePoints(int32 InAttributePoints);
+	virtual void AddToAttributePoints(int32 InAttributePoints) = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	void AddToSkillPointsBP(int32 InSkillPoints);
-	virtual void AddToSkillPoints(int32 InSkillPoints);
+	virtual void AddToSkillPoints(int32 InSkillPoints) = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	int32 FindLevelForExpBP(int32 InExp) const;
-	virtual int32 FindLevelForExp(int32 InExp) const;
+	virtual int32 FindLevelForExp(int32 InExp) const = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	int32 GetExpBP() const;
-	virtual int32 GetExp() const;
+	virtual int32 GetExp() const = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	int32 GetAttributePointsBP() const;
-	virtual int32 GetAttributePoints() const;
+	virtual int32 GetAttributePoints() const = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	int32 GetSkillPointsBP() const;
-	virtual int32 GetSkillPoints() const;
+	virtual int32 GetSkillPoints() const = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	int32 GetAttributePointsRewardBP(int32 PlayerLevel) const;
-	virtual int32 GetAttributePointsReward(int32 PlayerLevel) const;
+	virtual int32 GetAttributePointsReward(int32 PlayerLevel) const = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	int32 GetSkillPointsRewardBP(int32 PlayerLevel) const;
-	virtual int32 GetSkillPointsReward(int32 PlayerLevel) const;
+	virtual int32 GetSkillPointsReward(int32 PlayerLevel) const = 0;
 
 	UFUNCTION(BlueprintNativeEvent)
 	void LevelUp();
-	virtual void LevelUpCPP();
+	virtual void LevelUpCPP() = 0;
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void ShowDecalBP(); // 추후에 제거
